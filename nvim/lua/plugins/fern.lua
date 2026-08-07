6152aaeb2b0aad6c2c8f5766ef86dc4453c81ade
LegitYew0108 <legitlegit0108@gmail.com>
2026-08-07 23:38:51 +0900

    feat: apply colorscheme in fern

--- nvim/lua/plugins/fern.lua
+++ nvim/lua/plugins/fern.lua
@@ -15,24 +15,23 @@ return{
 			vim.g["glyph_palette#palette"] = require 'fr-web-icons'.palette()
 			vim.fn['glyph_palette#apply']()
 
-			vim.api.nvim_create_augroup('FernMyConf', {})
-			local ns = vim.api.nvim_create_namespace("fern-colors")
+			local group = vim.api.nvim_create_augroup('FernMyConf', {clear = true})
 
-			local colors = {
-				bg_dim = "#252630",
-				bg0 = "#2b2d3a",
-				bg1 = "#333648",
-				bg2 = "#363a4e",
-				bg3 = "#393e53",
-				bg4 = "#3f445b",
-				fg = "#c5cdd9",
-				blue = "#6cb6eb",
-				bg_blue = "#354157",
-				yellow = "#deb974",
-				grey_dim = "#535c6a"
-			}
+
+			local function set_fern_highlights()
+				vim.api.nvim_set_hl(0, 'FernRootSymbol', { link = "Directory" })
+				vim.api.nvim_set_hl(0, 'FernRootText', { link = "Directory" })
+				vim.api.nvim_set_hl(0, 'FernBranchSymbol', { link = 'Directory' })
+				vim.api.nvim_set_hl(0, 'FernBranchTextActive', { link = 'Directory' })
+				vim.api.nvim_set_hl(0, 'FernBranchTextInactive', { link = 'Comment' })
+				vim.api.nvim_set_hl(0, 'FernLeafSymbol', { link = "Normal" })
+				vim.api.nvim_set_hl(0, 'FernLeafTextActive', { link = "Normal" })
+				vim.api.nvim_set_hl(0, 'FernLeafTextInactive', { link = "Comment" })
+			end
+			set_fern_highlights()
 
 			vim.api.nvim_create_autocmd('FileType', {
+				group = group,
 				pattern = 'fern',
 				callback = function(args)
 					-- 相対と絶対の両方の行番号をオフにする
@@ -45,21 +44,8 @@ return{
 					vim.keymap.set("n","ff","<cmd>Fern . -reveal=% -drawer -toggle<CR>", { buffer = true })
 				end
 			})
-			vim.api.nvim_create_autocmd('User', {
-				group = 'FernMyConf',
-				pattern = 'FernHighlight',
-				callback = function()
-					vim.api.nvim_win_set_hl_ns(0, ns)
-					vim.api.nvim_set_hl(ns, 'FernBranchSymbol', { link = 'Directory' })
-					vim.api.nvim_set_hl(ns, 'FernBranchText', { link = 'Directory' })
-					vim.api.nvim_set_hl(ns, 'CursorLine', {
-						fg = colors.yellow,
-						bg = colors.bg3,
-					})
-				end
-			})
 			vim.api.nvim_create_autocmd('VimEnter', {
-				group = 'FernMyConf',
+				group = group,
 				nested = true,
 				callback = function(args)
 					if vim.fn.argc() > 0 then
@@ -70,7 +56,7 @@ return{
 				end
 			})
 			vim.api.nvim_create_autocmd('BufRead', {
-				group = 'FernMyConf',
+				group = group,
 				nested = true, --　必須
 				callback = function()
 					if vim.bo.filetype ~= "fern" and vim.bo.buftype == "" then
@@ -78,61 +64,37 @@ return{
 					end
 				end
 			})
-			vim.api.nvim_create_autocmd('BufEnter', {
-				group = 'FernMyConf',
-				callback = function(args)
+			vim.api.nvim_create_autocmd('WinEnter', {
+				group = group,
+				callback = function()
 					if vim.bo.filetype == "fern" then
 						if vim.g["should_reload_fern"] then
 							vim.g["should_reload_fern"] = false
 							vim.api.nvim_input("<F5>")
 						end
-						vim.api.nvim_win_set_hl_ns(0, ns)
-						vim.api.nvim_set_hl(ns, 'FernRootSymbol', { fg = colors.yellow })
-						vim.api.nvim_set_hl(ns, 'FernRootText', { fg = colors.yellow })
-						vim.api.nvim_set_hl(ns, 'FernBranchSymbol', { link = 'Directory' })
-						vim.api.nvim_set_hl(ns, 'FernBranchText', { link = 'Directory' })
-						vim.api.nvim_set_hl(ns, 'FernLeafText', { fg = colors.fg })
-						vim.api.nvim_set_hl(ns, 'Normal', {
-							bg = colors.bg_dim,
-						})
-						vim.api.nvim_set_hl(ns, 'FoldColumn', {
-							bg = colors.blue,
-						})
-						vim.api.nvim_set_hl(ns, 'EndOfBuffer', {
-							bg = colors.bg_dim,
-							fg = colors.bg_dim
-						})
-						vim.api.nvim_set_hl(ns, 'CursorLine', {
-							fg = colors.yellow,
-							bg = colors.bg4,
-						})
+						vim.wo.winhighlight = table.concat({
+							"FernBranchText:FernBranchTextActive",
+							"FernLeafText:FernLeafTextActive"
+						},",")
 					end
 				end
 			})
 
-			vim.api.nvim_create_autocmd('BufLeave', {
-				group = 'FernMyConf',
-				callback = function(args)
+			vim.api.nvim_create_autocmd('WinLeave', {
+				group = group,
+				callback = function()
 					if vim.bo.filetype == "fern" then
-						vim.api.nvim_set_hl(ns, 'FernRootSymbol', { fg = colors.fg })
-						vim.api.nvim_set_hl(ns, 'FernRootText', { fg = colors.fg })
-						vim.api.nvim_set_hl(ns, 'FernBranchSymbol', { fg = colors.fg })
-						vim.api.nvim_set_hl(ns, 'FernBranchText', { fg = colors.fg })
-						vim.api.nvim_set_hl(ns, 'FernLeafText', { fg = colors.fg })
-						vim.api.nvim_win_set_hl_ns(0, ns)
-						vim.api.nvim_set_hl(ns, 'Normal', {
-							bg = colors.bg0,
-						})
-						vim.api.nvim_set_hl(ns, 'EndOfBuffer', {
-							bg = colors.bg0,
-							fg = colors.bg0
-						})
-						vim.api.nvim_set_hl(ns, 'CursorLine', {
-							fg = colors.yellow,
-							bg = colors.bg4,
-						})
+						vim.wo.winhighlight = table.concat({
+							"FernBranchText:FernBranchTextInactive",
+							"FernLeafText:FernLeafTextInactive"
+						},",")
 					end
 				end
+			})
+
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				group = group,
+				callback = set_fern_highlights
 			})
 		end,
 	}
