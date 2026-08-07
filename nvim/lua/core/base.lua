238225e1785127ebe0aa1a5b95249a973ad3d448
LegitYew0108 <legitlegit0108@gmail.com>
2026-08-07 23:39:41 +0900

    feat: add for typst and tex

--- nvim/lua/core/base.lua
+++ nvim/lua/core/base.lua
@@ -9,11 +9,22 @@ vim.o.expandtab = false
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
--- pdf typst
+vim.diagnostic.config({
+	float = {
+		wrap = true,
+		max_width = 80,
+	}
+})
+
+-- pdf typst and tex
 vim.api.nvim_create_user_command("OpenPdf", function()
   local filepath = vim.api.nvim_buf_get_name(0)
   if filepath:match("%.typ$") then
     local pdf_path = filepath:gsub("%.typ$", ".pdf")
+    vim.system({ "open", pdf_path })
+  end
+  if filepath:match("%.tex$") then
+    local pdf_path = filepath:gsub("%.tex$", ".pdf")
     vim.system({ "open", pdf_path })
   end
 end, {})
